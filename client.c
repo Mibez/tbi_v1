70c6a0c840125c790242623219682c1d8eba238a
Miikka Lukumies <miikka.lukumies@hotmail.com>
2024-01-22 21:05:33 +0200

    Update client for DCB

--- client.c
+++ client.c
@@ -4,18 +4,28 @@
 *           before compiling 
 */
 
+#define _DEFAULT_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
 
 #include "tbi.h"
 #include "messagespec.h"
 
+/** @brief Flag to stop looping and its sig handler */
+static sig_atomic_t stopping = 0;
+void sig_handler(int signum) {
+    stopping = 1;
+}
 
 int main(int argc, char* arv[])
 {
     tbi_ctx_t* tbi;
     int ret;
-    
+
+    signal(SIGINT, sig_handler); 
+
     tbi = tbi_init();
     if(!tbi)
         return 1;
@@ -31,12 +41,12 @@ int main(int argc, char* arv[])
     /* Telemetry can be either dynamically or statically allocated, it's copied to internal buffers */
     msgspec_temp_and_hum_t *temp1 = (msgspec_temp_and_hum_t *)malloc(sizeof(msgspec_temp_and_hum_t));
     msgspec_temp_and_hum_t temp2 = {
-        .time = 0xdeadbeef,
+        .time_s = 0xdeadbeef,
         .temp = 0xbaad1dea,
         .hum  = 0xff,
     };
     
-    temp1->time = 0xaabbccdd;
+    temp1->time_s = 0xaabbccdd;
     temp1->temp = 0x11223344;
     temp1->hum = 0xff;
 
@@ -49,8 +59,47 @@ int main(int argc, char* arv[])
 
     free(temp1);
 
+
+    /* Schedule a lot of messages for DCB */
+    msgspec_acceleration_t acc;
+    acc = (msgspec_acceleration_t){.time_s = 0, .time_ms = 900, .acc_x = 777, .acc_y = 6666, .acc_z = 1 };
+    if((ret = tbi_send_acceleration(tbi, &acc)) != 0)
+        goto exit_alloc;
+    
+    acc = (msgspec_acceleration_t){.time_s = 10, .time_ms = 654, .acc_x = 8096, .acc_y = 7777, .acc_z = 2 };
+    if((ret = tbi_send_acceleration(tbi, &acc)) != 0)
+        goto exit_alloc;
+
+    acc = (msgspec_acceleration_t){.time_s = 18, .time_ms = 322, .acc_x = 999, .acc_y = 200, .acc_z = -3 };
+    if((ret = tbi_send_acceleration(tbi, &acc)) != 0)
+        goto exit_alloc;
+
+    acc = (msgspec_acceleration_t){.time_s = 100, .time_ms = 1, .acc_x = 100, .acc_y = 5656, .acc_z = 4 };
+    if((ret = tbi_send_acceleration(tbi, &acc)) != 0)
+        goto exit_alloc;
+
+    acc = (msgspec_acceleration_t){.time_s = 199, .time_ms = 40, .acc_x = 999, .acc_y = 5555, .acc_z = 5 };
+    if((ret = tbi_send_acceleration(tbi, &acc)) != 0)
+        goto exit_alloc;
+
+    acc = (msgspec_acceleration_t){.time_s = 250, .time_ms = 888, .acc_x = 999, .acc_y = 6666, .acc_z = 6 };
+    if((ret = tbi_send_acceleration(tbi, &acc)) != 0)
+        goto exit_alloc;
+
+    acc = (msgspec_acceleration_t){.time_s = 300, .time_ms = 555, .acc_x = 999, .acc_y = 7777, .acc_z = 7 };
+    if((ret = tbi_send_acceleration(tbi, &acc)) != 0)
+        goto exit_alloc;
+
+    acc = (msgspec_acceleration_t){.time_s = 356, .time_ms = 123, .acc_x = 999, .acc_y = -8888, .acc_z = -8 };
+    if((ret = tbi_send_acceleration(tbi, &acc)) != 0)
+        goto exit_alloc;
+
     printf("Calling process() to send telemetry...\n");
-    while((ret = tbi_client_process(tbi)) > 0) {;}
+
+    while(!stopping) {
+        ret = tbi_client_process(tbi);
+        usleep(100*1000);
+    }
 
     tbi_close(tbi);
 
